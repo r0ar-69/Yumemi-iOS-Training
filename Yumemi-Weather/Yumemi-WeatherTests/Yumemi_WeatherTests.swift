06b8a8a1ea904f816479cc0ae584f1f876b15ec0
r0ar-69 <shun199969kiyo@gmail.com>
2021-06-01 13:06:28 +0900

    init

---
+++ Yumemi-Weather/Yumemi-WeatherTests/Yumemi_WeatherTests.swift
@@ -0,0 +1,33 @@
+//
+//  Yumemi_WeatherTests.swift
+//  Yumemi-WeatherTests
+//
+//  Created by 清浦 駿 on 2021/06/01.
+//
+
+import XCTest
+@testable import Yumemi_Weather
+
+class Yumemi_WeatherTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
