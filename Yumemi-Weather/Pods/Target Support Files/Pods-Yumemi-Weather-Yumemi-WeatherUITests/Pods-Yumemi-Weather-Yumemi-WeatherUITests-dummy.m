e11be8f35d12885c9828d44e369af415488fa62b
r0ar-69 <shun199969kiyo@gmail.com>
2021-06-04 12:14:30 +0900

    Pods配下を削除

--- Yumemi-Weather/Pods/Target Support Files/Pods-Yumemi-Weather-Yumemi-WeatherUITests/Pods-Yumemi-Weather-Yumemi-WeatherUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Yumemi_Weather_Yumemi_WeatherUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Yumemi_Weather_Yumemi_WeatherUITests
-@end
--- Yumemi-Weather/Pods/Target Support Files/Pods-Yumemi-Weather/Pods-Yumemi-Weather-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Yumemi_Weather : NSObject
-@end
-@implementation PodsDummy_Pods_Yumemi_Weather
-@end
